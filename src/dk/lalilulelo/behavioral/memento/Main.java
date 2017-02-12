0dcad41c2cacc9e9c0f754169c2914f42600b76d
Rasmus Højte <rasmusvh@msn.com>
2017-02-12 01:00:46 +0100

    Added mains

---
+++ src/dk/lalilulelo/behavioral/memento/Main.java
@@ -0,0 +1,10 @@
+package dk.lalilulelo.behavioral.memento;
+
+/**
+ * Created by Rasmu on 2/12/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/dk/lalilulelo/creational/factory_method/Main.java
@@ -0,0 +1,10 @@
+package dk.lalilulelo.creational.factory_method;
+
+/**
+ * Created by Rasmu on 2/12/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
